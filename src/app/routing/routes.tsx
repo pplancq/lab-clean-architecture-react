1b9db19c0ea1a70e8e08da1c20a9e29769d2d3e9
Paul PLANCQ <paul.plancq@outlook.fr>
2026-03-17 14:50:33 +0100

    feat(app-shell): add AppShell component to main route

--- src/app/routing/routes.tsx
+++ src/app/routing/routes.tsx
@@ -2,11 +2,13 @@ import { addGameRoutes } from '@Collection/ui/pages/AddGame';
 import { editGameRoutes } from '@Collection/ui/pages/EditGame';
 import { gameDetailRoutes } from '@Collection/ui/pages/GameDetail';
 import { homeRoutes } from '@Collection/ui/pages/Home';
+import { AppShell } from '@Shared/ui/components/AppShell/AppShell';
 import type { RouteObject } from 'react-router';
 
 export const routeObject: RouteObject[] = [
   {
     path: '/',
+    element: <AppShell />,
     children: [homeRoutes, addGameRoutes, gameDetailRoutes, editGameRoutes],
   },
 ];
